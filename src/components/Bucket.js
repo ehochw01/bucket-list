11647247e61f87212708ea75dfcfe093b66890a8
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2022-11-15 13:01:00 -0500

    completed bucket.js

--- src/components/Bucket.js
+++ src/components/Bucket.js
@@ -12,9 +12,12 @@ function Bucket(props) {
 
   const submitUpdate = (value) => {
 
-    // TODO: Write logic to update the `edit` value in state after a user updates an entry in the list
-
-    // TODO: Set the key:value pairs in the `edit` object back to empty strings
+    props.editBucketItem(edit.id, value);
+    setEdit({
+      id: null,
+      value: '',
+      eagerness: '',
+    });
 
   };
 
@@ -27,17 +30,21 @@ function Bucket(props) {
     // TODO: Add a className of `bucket row complete ${item.eagerness}` for completed items, and `bucket-row ${item.eagerness}` for non-completed items
     // TODO: Add a key attribute set to the value of the index position
     // Hint: use a ternary operator
-    <div className={ } key={}>
+    <div className={item.isComplete ? `bucket row complete ${item.eagerness}`: `bucket-row ${item.eagerness}`} key={index}>
 
-      // TODO: Add an onClick event that invokes the `completeBucketItem` method passing the item id as a argument
-      <div key={} onClick={}>
-          {/* TODO: Add the item text here */}
+      {/* TODO: Add an onClick event that invokes the `completeBucketItem` method passing the item id as a argument */}
+      <div key={item.id} onClick={props.completeBucketItem(item.id)}>
+          {item.text}
       </div>
       <div className="icons">
-        // TODO: Add an onClick event update the `edit` object with the `id`, `value`, and `eagerness` properties
-        <p onClick={}> ✏️</p>
+        {/* // TODO: Add an onClick event update the `edit` object with the `id`, `value`, and `eagerness` properties */}
+        <p onClick={setEdit({
+          id: item.id,
+          value: item.text,
+          eagerness: item.eagerness
+        })}> ✏️</p>
         {/* TODO: Add an onClick event that will invoke the removeBucketItem method passing in the `item.id` */}
-        <p onClick={}> 🗑️</p>
+        <p onClick={props.removeBucketItem(item.id)}> 🗑️</p>
       </div>
     </div>
   ));
